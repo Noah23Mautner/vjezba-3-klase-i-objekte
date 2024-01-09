cebd4dc3115fd8e72951a82b0f3c71f69ef6fe14
Noah23Mautner <152974783+Noah23Mautner@users.noreply.github.com>
2024-01-09 12:33:27 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace vj._3_klase_i_objekti
+{
+    public class Stablo
+    {
+        private bool otpadajuListovi;
+        private string vrstaStabla;
+
+        public bool OtpadajuListovi
+        {
+            get { return otpadajuListovi; }
+            set { otpadajuListovi = value; }
+        }
+
+        public Stablo(bool otpadajuListovi)
+        {
+            this.otpadajuListovi = otpadajuListovi;
+        }
+
+        public override string ToString()
+        {
+            if (otpadajuListovi)
+            {
+                vrstaStabla = "bijelogorično";
+            }
+            else
+            {
+                vrstaStabla = "crnogorično";
+            }
+
+            return $"Vrsta stabla: {vrstaStabla}, Otpadaju listovi: {otpadajuListovi}";
+        }
+    }
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Stablo stablo1 = new Stablo(false);
+            Stablo stablo2 = new Stablo(true);
+
+            Console.WriteLine(stablo1.ToString());
+            Console.WriteLine(stablo2.ToString());
+
+            Console.ReadKey();
+        }
+    }
+
+}
